90e3ff7cd54106fb38ba62918c6830a6e37add1d
Andrew vonderLuft <avonderluft@avlux.net>
2011-04-04 12:38:44 -0700

    don't add published data to the drawer - it's already on page-edit

--- concurrent_draft_extension.rb
+++ concurrent_draft_extension.rb
@@ -4,7 +4,7 @@ require_dependency 'application_controller'
 class ConcurrentDraftExtension < Radiant::Extension
   version "#{File.read(File.expand_path(File.dirname(__FILE__)) + '/VERSION')}"
   description "Enables default draft versions of pages, snippets and layouts, which can be scheduled for promotion to Production"
-  url "https://github.com/avonderluft/radiant-concurrent_draft-extension/tree/master"
+  url "https://github.com/avonderluft/radiant-concurrent_draft-extension"
 
   def activate
     [Page, Snippet, Layout, PagePart].each do |klass|
@@ -24,7 +24,7 @@ class ConcurrentDraftExtension < Radiant::Extension
       # admin.send(view).edit.form_bottom.delete 'edit_buttons'
       # admin.send(view).edit.add :form_bottom, 'admin/edit_buttons'
     end
-    admin.page.edit.add :extended_metadata, 'published_meta'
+    # admin.page.edit.add :extended_metadata, 'published_meta'
     Time::DATE_FORMATS[:long_civilian] = lambda {|time| time.strftime("%B %d, %Y %I:%M%p").gsub(/(\s+)0(\d+)/, '\1\2') }
   end
 end
